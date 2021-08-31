fd7e4aadb438cbd8a47884c2a580984c38518c95
oguz gül <ogzgl95@outlook.com>
2021-08-31 12:50:22 +0300

    Creaete header side and build fetching API data

--- src/App.js
+++ src/App.js
@@ -1,9 +1,39 @@
+import { FormControl, MenuItem, Select } from "@material-ui/core";
+import { useState, useEffect } from "react";
 import "./App.css";
 
 function App() {
-  return <div className="app">
-    <h1>Let's build a Covid 19 Tracker App 🚀</h1>
-  </div>;
+  const [countries, setCountries] = useState([]);
+
+  useEffect(() => {
+    const getCountriesData = async () => {
+      await fetch("https://disease.sh/v3/covid-19/countries")
+        .then((response) => response.json())
+        .then((data) => {
+          const countries = data.map((country) => ({
+            name: country.country,
+            value: country.countryInfo.iso2,
+          }));
+          setCountries(countries);
+        });
+    };
+    getCountriesData();
+  }, []);
+
+  return (
+    <div className="app">
+      <div className="app__header">
+        <h1>COVID-19 TRACKER</h1>
+        <FormControl className="app__dropdown">
+          <Select variant="outlined" value="abc">
+            {countries.map((country) => (
+              <MenuItem value={country.value}>{country.name}</MenuItem>
+            ))}
+          </Select>
+        </FormControl>
+      </div>
+    </div>
+  );
 }
 
 export default App;
